b0b5fd8b83a0ffa3fbca1757119d181aabad4b46
Dave Weinberg <dave@gentlyventures.com>
2025-06-23 22:47:50 -0400

    updated

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,8 +2,13 @@ module.exports = {
   projects: [
     '<rootDir>/packages/core',
     '<rootDir>/packages/portia-adapter',
-    '<rootDir>/packages/init-phase'
+    '<rootDir>/packages/init-phase',
+    '<rootDir>/packages/langgraph-adapter',
+    '<rootDir>/tools'
   ],
-  testMatch: ['**/__tests__/**/*.test.[jt]s?(x)'],
+  testMatch: [
+    '**/__tests__/**/*.test.[jt]s?(x)',
+    '**/tests/integration/**/*.test.[jt]s?(x)'
+  ],
   testEnvironment: 'node'
 };
