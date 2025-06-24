f8a78f92b4b9d154986f8f2b6ebf1a3c036dcd9d
Dave Weinberg <dave@gentlyventures.com>
2025-06-23 22:53:31 -0400

    chore(test): include integration tests in Jest config

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,7 +2,10 @@ module.exports = {
   projects: [
     '<rootDir>/packages/core',
     '<rootDir>/packages/portia-adapter',
-    '<rootDir>/packages/init-phase'
+    '<rootDir>/packages/init-phase',
+    '<rootDir>/packages/langgraph-adapter',
+    '<rootDir>/tools',
+    '<rootDir>/tests/integration'
   ],
   testMatch: ['**/__tests__/**/*.test.[jt]s?(x)'],
   testEnvironment: 'node'
